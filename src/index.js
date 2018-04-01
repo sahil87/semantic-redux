384774172ecf30a5bfa04cbdd3da9c9bf9818334
Sahil Ahuja <sahilahuja@gmail.com>
2018-04-01 20:07:22 +0530

    Redux boilerplate

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "./index.css";
 import "semantic-ui-css/semantic.min.css";
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import store from "./store";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
+  registerServiceWorker();
+  
